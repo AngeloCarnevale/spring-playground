b72e5c40adc560b3af725aed725ca75e66e8ceb6
AngeloCarnevale <angelo.carnevale@hotmail.com>
2024-03-13 10:57:29 -0300

    feat: create transaction repository

---
+++ bank/src/main/java/com/bank/repositories/TransactionRepository.java
@@ -0,0 +1,7 @@
+package com.bank.repositories;
+
+import com.bank.domain.transaction.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+}
