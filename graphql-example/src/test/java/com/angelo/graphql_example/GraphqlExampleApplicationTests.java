d14c06635e785887e717bf01c0c59643ae084895
AngeloCarnevale <angelo.carnevale@hotmail.com>
2024-06-06 10:55:55 -0300

    feat: create graphql project

---
+++ graphql-example/src/test/java/com/angelo/graphql_example/GraphqlExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.angelo.graphql_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
